4bff54049a63e8f6c3d03719a6fef3e2787bbb28
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-11-18 08:53:53 +0530

    add form container

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,25 @@
+// 'use client';
+
+// import { useFormState } from 'react-dom';
+// import { useEffect } from 'react';
+// import { useToast } from '@/components/ui/use-toast';
+// import { actionFunction } from '@/utils/types';
+// import { init } from 'next/dist/compiled/webpack/webpack';
+
+// const initialState = {
+//   message: '',
+// };
+
+// function FormContainer({
+//   action,
+//   children,
+// }: {
+//   action: actionFunction;
+//   children: React.ReactNode;
+// }) {
+//   const [state, formAction] = useFormState(action, initialState);
+
+//   return <form action={}>{children}</form>;
+// }
+
+// // export default FormContainer;
