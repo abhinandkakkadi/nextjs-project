6b5344b04adc3e69ed32cde12cd01f584fd57867
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-12-04 09:08:31 +0530

    add update profile page and functionality

--- utils/actions.ts
+++ utils/actions.ts
@@ -8,6 +8,18 @@ import db from './db';
 import { clerkClient, currentUser } from '@clerk/nextjs/server';
 // import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('protected route');
+  }
+
+  if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+
+  return user;
+};
 
 export const createProfileAction = async (
   prevState: any,
@@ -17,12 +29,10 @@ export const createProfileAction = async (
     const user = await currentUser();
     if (!user) throw new Error('Please login to create a profile');
 
-    console.log(user);
-
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
 
-    db.profile.create({
+    await db.profile.create({
       data: {
         clerkId: user.id,
         email: user.emailAddresses[0].emailAddress,
@@ -38,14 +48,74 @@ export const createProfileAction = async (
       },
     });
   } catch (error) {
-    console.log(error);
-    return {
-      message:
-        error instanceof Error
-          ? error.message
-          : 'An unexpected error has occurred',
-    };
+    return renderError(error);
   }
 
   redirect('/');
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+
+  return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) redirect('profile/create');
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await currentUser();
+  if (!user) throw new Error('create a user to update user');
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath('/profile');
+    return { message: 'profile updated' };
+  } catch (error) {
+    console.log(error);
+    return renderError(error);
+  }
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message:
+      error instanceof Error
+        ? error.message
+        : 'An unexpected error has occurred',
+  };
 };
