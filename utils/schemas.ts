9bbbc50bc59821249b487fe9a4e7d3fd1cac04a3
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-12-05 09:07:41 +0530

    implemented zod parsing generic method

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,9 +1,29 @@
 import * as z from 'zod';
-// import { ZodSchema } from 'zod';
+import { ZodSchema } from 'zod';
 
 export const profileSchema = z.object({
   // firstName: z.string().max(5, { message: 'max length is 5' }),
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  username: z.string().min(2),
+  firstName: z
+    .string()
+    .min(2, { message: 'first name should contain at least 2 characters' }),
+  lastName: z
+    .string()
+    .min(2, { message: 'last name should contain at least 2 characters' }),
+  username: z
+    .string()
+    .min(2, { message: 'username should contain at least 2 characters' }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const validatedFields = schema.safeParse(data);
+
+  if (!validatedFields.success) {
+    const errors = validatedFields.error.errors.map((error) => error.message);
+    throw new Error(errors.join(','));
+  }
+
+  return validatedFields.data;
+}
